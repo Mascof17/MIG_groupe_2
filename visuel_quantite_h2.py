1752fb53b892ff647696573f3c24550e60f6eaaf
Mascof17 <maxdebussac@gmail.com>
2023-12-01 11:32:47 +0100

    Programme visualisation stockage

---
+++ visuel_quantite_h2.py
@@ -0,0 +1,27 @@
+from Taha import 
+from Louis import
+import matplotlib.pyplot as plt
+
+
+Pstockage=
+Minit_stockage=
+
+Pinit_MP1, Pinit_MP1=500
+Pinit_MP2, Pinit_MP1=500
+Pinit_MP3, Pinit_MP1=500
+Pinit_MP4, Pinit_MP1=500
+
+Pinit_LP1, Pinit_MP1=350
+Pinit_LP2, Pinit_MP1=350
+Pinit_LP3, Pinit_MP = 1350
+Pinit_LP4, Pinit_MP1 = 350
+
+
+nb_passages_bus= 22     #par jour
+nb_passages_trains=  0     #par an
+
+reservoir_bus=  35    #en kg
+reservoir_train=  215    #en kg
+
+
+
