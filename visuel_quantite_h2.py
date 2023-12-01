0fa822d4f29f26223b5d64f8209a8fba86d83997
Mascof17 <maxdebussac@gmail.com>
2023-12-01 12:02:55 +0100

    OK

--- visuel_quantite_h2.py
+++ visuel_quantite_h2.py
@@ -1,20 +1,21 @@
-from Taha import 
-from Louis import
+
 import matplotlib.pyplot as plt
+import CoolProp.CoolProp as cp
 
 
-Pstockage=
-Minit_stockage=
+n_stockage=3
+Pstockage=300
+Minit_stockage=350
 
-Pinit_MP1, Pinit_MP1=500
-Pinit_MP2, Pinit_MP1=500
-Pinit_MP3, Pinit_MP1=500
-Pinit_MP4, Pinit_MP1=500
+Pinit_MP1, Minit_MP1=450, 50
+Pinit_MP2, Minit_MP2=450, 50
+Pinit_MP3, Minit_MP3=450, 50
+Pinit_MP4, Minit_MP4=450, 50
 
-Pinit_LP1, Pinit_MP1=350
-Pinit_LP2, Pinit_MP1=350
-Pinit_LP3, Pinit_MP = 1350
-Pinit_LP4, Pinit_MP1 = 350
+Pinit_LP1, Minit_LP1=30, 50
+Pinit_LP2, Minit_LP2=300, 50
+Pinit_LP3, Minit_LP3 = 300, 50
+Pinit_LP4, Minit_LP4 = 300, 50
 
 
 nb_passages_bus= 22     #par jour
@@ -22,6 +23,11 @@ nb_passages_trains=  0     #par an
 
 reservoir_bus=  35    #en kg
 reservoir_train=  215    #en kg
+
+#le compresseur ne fontionne que pour les middle pressure storages
+#Je vais donc faire à la main pour les low pressure storage
+
+
 
 
 
