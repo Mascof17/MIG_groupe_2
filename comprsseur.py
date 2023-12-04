b14926d72b1298a2854230b34bbaf913fb6a8a21
Mascof17 <maxdebussac@gmail.com>
2023-12-04 12:08:32 +0100

    petites modifs

--- comprsseur.py
+++ comprsseur.py
@@ -2,7 +2,7 @@ import CoolProp.CoolProp as cp
 import matplotlib.pyplot as plt
 T = 293
 debit_normo =300/3600 #300Nm^3/h  donc 300/3600 par seconde source: https://fr.wikipedia.org/wiki/Compresseur_d%27hydrog%C3%A8ne
-P= 300*100000 #pression initialement à 300 bars
+P= 300*101300 #pression initialement à 300 bars
 p_in=cp.PropsSI('Dmass' ,'T', T,'P', P , 'H2')
 print(p_in)
 V= 14.8 #volume dans le reservoir 
@@ -11,35 +11,31 @@ X.append(p_in)
 Temps = [0]
 dt=0.1
 print(P)
-Pression = [P/100000]
+Pression = [P/101300]
 P_out = [50] #Pression dans le reservoir de Casacade
 V_out = 1.7 #volume en metre cube dans le reservoir   de cascade 2.4 LP, 1.7 MP
-masse_volumique_out = [cp.PropsSI('Dmass' ,'T', T,'P', P_out[0]*100000 , 'H2')]
+masse_volumique_out = [cp.PropsSI('Dmass' ,'T', T,'P', P_out[0]*101300 , 'H2')]
 masse_out = [masse_volumique_out[0]*V_out]
-debit_max = 0
 
 while P_out[-1]<500 : #on s'arrete à MP a 500bars
     debit_volumique = (debit_normo*1.01325*(T+273))/(Pression[-1]*273) #source pour la formule https://www.detendeur.fr/m3h.normo.m3h.p.html
     debit_massique = X[-1]*debit_volumique
-    if debit_massique > debit_max:
-        debit_max=debit_massique
     X.append(X[-1]-debit_massique*dt/V)  # derivee de la masse vaut -debit, m[i+1]=m[i]-debit*dt
     P= cp.PropsSI('P', 'T', T, 'Dmass', X[-1], 'H2')
     masse_out.append(masse_out[-1]+debit_massique*dt)
     masse_volumique_out.append(masse_out[-1]/V_out)
-    P_out.append((cp.PropsSI('P', 'T', T, 'Dmass', masse_volumique_out[-1], 'H2'))/100000)
-    Pression.append(P/100000)
+    P_out.append((cp.PropsSI('P', 'T', T, 'Dmass', masse_volumique_out[-1], 'H2'))/101300)
+    Pression.append(P/101300)
     Temps.append(Temps[-1]+dt)
+    
+"""
 print(Temps[-1])
 print((X[0]-X[-1])*V)
-print(debit_max) #debit max en kg/s
-print(debit_max*3600) #debit max en kg/h
-
 plt.plot(Temps,Pression)
 plt.xlabel('temps')
 plt.ylabel(" pression d'H2 dans le stockage")
 plt.show()
-
+"""
 Masse=[]
 for i in range(len(X)):
     Masse.append(V*X[i])
@@ -48,10 +44,12 @@ plt.xlabel('temps')
 plt.ylabel(" Masse d'H2 dans le stockage")
 plt.show()
 
+"""
 plt.plot(Temps, P_out)
 plt.xlabel('temps (en secondes)')
 plt.ylabel(" pression d'H2 dans la cascade (en bar)")
 plt.show()
+"""
 
 plt.plot(Temps,masse_out)
 plt.xlabel('temps')
--- visuel_quantite_h2.py
+++ visuel_quantite_h2.py
@@ -1,7 +1,9 @@
 
 import matplotlib.pyplot as plt
 import CoolProp.CoolProp as cp
+import numpy as np
 import math as m
+import comprsseur as cp
 
 
 T=293  #K
@@ -17,18 +19,19 @@ l_m_MP=[50]*4
 l_P_MP=[450e5]*4
 V_MP = l_m_MP[0]/cp.PropsSI('Dmass' ,'T', 293,'P', l_P_MP, 'H2')
 
+
 l_m_LP=[50]*4
 l_P_LP=[300e5]*4
 V_LP = l_m_LP[0]/cp.PropsSI('Dmass' ,'T', T,'P', l_P_LP[0], 'H2')
 
 print(v_in, V_MP)
 
-nb_passages_bus= 22     #par jour
+nb_passages_bus= 7    #par jour
 nb_passages_trains=  0     #par an
 
-reservoir_bus=  35    #en kg
-reservoir_train=  215    #en kg
-
+reservoirs_bus =  35    #en kg
+reservoir_train =  215    #en kg
+V_bus =
 #le compresseur ne fontionne que pour les middle pressure storages
 #Je vais donc faire à la main pour les low pressure storage
 
@@ -46,35 +49,90 @@ print(f"c'est{l_m_LP[0]/V_LP}")
 #étudions désormais une fonction qui suit la quantité d'hydrogène dans le LP tank
 
 def remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP):
-    dt = 0.01
+    dt = 0.0000003
     t = 0
-    for i in range (len(l_m_LP)):
-        
-        for n in range (len(l_m_stock)):
-            kp = 0.035
-            pressure =cp.PropsSI('P' ,'T', T,'Dmass', l_m_stock[n]/v_in, 'H2')
-            while l_m_LP[i]< 50 and l_m_stock[n] > 60:
-                print(l_m_stock)
-                rho = cp.PropsSI('Dmass' ,'T', T, 'P', pressure, 'H2')
-                dm = dt*rho*m.sqrt((cp.PropsSI('P' ,'T', T,'Dmass', l_m_stock[n]/v_in, 'H2') - cp.PropsSI('P' ,'T', T,'Dmass', l_m_LP[i]/V_LP, 'H2'))*2/kp*rho)
-                print(dm/(dt*rho))
+    sum_P=0
+    for i in range (len(l_m_LP)):  #on parcourt le stck LP
+        for n in range (len(l_m_stock)):   #on parcourt les stocks livrés
+            kp = 0.035  #pas important on considérera le chargement inférieur à 15 min
+            pressure = cp.PropsSI('P' ,'T', T,'Dmass', l_m_stock[n]/v_in, 'H2')
+            while l_m_LP[i]< 50 and l_m_stock[n] > 60 and pressure - cp.PropsSI('P' ,'T', T,'Dmass', l_m_LP[i]/V_LP, 'H2')) > 0:
+                rho = cp.PropsSI('Dmass' ,'T', T, 'P', pressure, 'H2')  
+                dm = dt*rho*m.sqrt((cp.PropsSI('P' ,'T', T,'Dmass', l_m_stock[n]/v_in, 'H2') - cp.PropsSI('P' ,'T', T,'Dmass', l_m_LP[i]/V_LP, 'H2'))*2/kp*rho) #formule du débit mais ne fonctionne pas bien
                 l_m_LP[i] += dm
                 l_m_stock[n] -= dm
-                t += 1
+                #sum_P += 
                 
-    
+
     return (l_m_stock,l_m_LP)
 
+t=np.linspace(4*24, 4*24)    #on découpe la journée en quarts d'heure
+stock_tab = np.zeros(4*24, 3)   #tableau des données dans les stockages en f(t)
+stock_tab[0] = l_m_stock
+LP_tab = np.zeros(4*24, 4) #idem pour LP
+LP_tab[0] = l_m_LP
+MP_tab = np.zeros(4*24, 4) #idem pour MP
+MP_tab[0] = l_m_MP
+for i in range (1, len(t)):
+    if t % 8 == 7:
+        l_m_stock,l_m_LP, l_m_MP = remplissage_bus(T, l_m_LP, l_m_MP, V_bus, v_in, V_LP, V_MP, reservoirs_bus)
+    else:
+        l_m_stock,l_m_LP = remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP)
+        l_m_stock, l_m_MP = remplissage_MP_15min(T, l_m_MP, l_m_stock, v_in, V_LP)
+    LP_tab[i] = l_m_LP
+    MP_tab[i] = l_m_MP
+    stock_tab[i] = l_m_stock
+
+#l_m_stock,l_m_LP = remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP)
 
 Minit_LP1 = cp.PropsSI('Dmass' ,'T', T,'P', 50e5, 'H2') * V_LP
-l_m_LP=[Minit_LP1]*4
+l_m_LP = [Minit_LP1]*4
 
 print(l_m_LP)
 
-print (remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP)) #attention le débit est bcp trop important
+print (f"l_m_stock={remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP)[0]} et l_m_LP={remplissage_LP(T, l_m_LP, l_m_stock, v_in, V_LP)[1]}") #attention le débit est bcp trop important
 
 
 
 
 
 
+#Création des subplots pour visualiser la quantité
+fig, axs = plt.subplots(2, 4, figsize=(12, 6))
+
+# Sous-plot 1
+axs[0, 0].plot(t, l_m_stock[::, 0])
+axs[0, 0].set_title('Sin(x)')
+
+# Sous-plot 2
+axs[0, 1].plot(t, l_m_stock[::, 1])
+axs[0, 1].set_title('Cos(x)')
+
+# Sous-plot 3
+axs[0, 2].plot(t, l_m_stock[::, 2])
+axs[0, 2].set_title('Tan(x)')
+
+# Sous-plot 4
+axs[0, 3].plot(t, l_m_LP[::, 0])
+axs[0, 3].set_title('x^2')
+
+# Sous-plot 5
+axs[1, 0].plot(t, l_m_LP[::, 1])
+axs[1, 0].set_title('exp(x)')
+
+# Sous-plot 6
+axs[1, 1].plot(t, l_m_LP[::, 2])
+axs[1, 1].set_title('log(x+1)')
+
+# Sous-plot 7
+axs[1, 2].plot(t, l_m_LP[::, 3])
+axs[1, 2].set_title('sqrt(x)')
+
+# Supprimer le huitième sous-plot (il n'y a pas assez de sous-plots pour 2x4)
+fig.delaxes(axs[1, 3])
+
+# Ajuster automatiquement les espacements pour éviter le chevauchement
+plt.tight_layout()
+
+# Afficher le graphique
+plt.show()
