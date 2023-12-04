17cec0703b905b84b91655a3f697a1f6493c03b4
LouisBindel <144105060+LouisBindel@users.noreply.github.com>
2023-12-04 10:50:49 +0100

    Add files via upload

--- comprsseur.py
+++ comprsseur.py
@@ -16,10 +16,13 @@ P_out = [50] #Pression dans le reservoir de Casacade
 V_out = 1.7 #volume en metre cube dans le reservoir   de cascade 2.4 LP, 1.7 MP
 masse_volumique_out = [cp.PropsSI('Dmass' ,'T', T,'P', P_out[0]*101300 , 'H2')]
 masse_out = [masse_volumique_out[0]*V_out]
+debit_max = 0
 
 while P_out[-1]<500 : #on s'arrete à MP a 500bars
     debit_volumique = (debit_normo*1.01325*(T+273))/(Pression[-1]*273) #source pour la formule https://www.detendeur.fr/m3h.normo.m3h.p.html
     debit_massique = X[-1]*debit_volumique
+    if debit_massique > debit_max:
+        debit_max=debit_massique
     X.append(X[-1]-debit_massique*dt/V)  # derivee de la masse vaut -debit, m[i+1]=m[i]-debit*dt
     P= cp.PropsSI('P', 'T', T, 'Dmass', X[-1], 'H2')
     masse_out.append(masse_out[-1]+debit_massique*dt)
@@ -29,6 +32,8 @@ while P_out[-1]<500 : #on s'arrete à MP a 500bars
     Temps.append(Temps[-1]+dt)
 print(Temps[-1])
 print((X[0]-X[-1])*V)
+print(debit_max) #debit max en kg/s
+print(debit_max*3600) #debit max en kg/h
 
 plt.plot(Temps,Pression)
 plt.xlabel('temps')
