bcada86f90fb24c6a66a844b329fcf2fb386aa51
LouisBindel <144105060+LouisBindel@users.noreply.github.com>
2023-12-04 11:02:06 +0100

    Add files via upload

--- comprsseur.py
+++ comprsseur.py
@@ -2,7 +2,7 @@ import CoolProp.CoolProp as cp
 import matplotlib.pyplot as plt
 T = 293
 debit_normo =300/3600 #300Nm^3/h  donc 300/3600 par seconde source: https://fr.wikipedia.org/wiki/Compresseur_d%27hydrog%C3%A8ne
-P= 300*101300 #pression initialement à 300 bars
+P= 300*100000 #pression initialement à 300 bars
 p_in=cp.PropsSI('Dmass' ,'T', T,'P', P , 'H2')
 print(p_in)
 V= 14.8 #volume dans le reservoir 
@@ -11,10 +11,10 @@ X.append(p_in)
 Temps = [0]
 dt=0.1
 print(P)
-Pression = [P/101300]
+Pression = [P/100000]
 P_out = [50] #Pression dans le reservoir de Casacade
 V_out = 1.7 #volume en metre cube dans le reservoir   de cascade 2.4 LP, 1.7 MP
-masse_volumique_out = [cp.PropsSI('Dmass' ,'T', T,'P', P_out[0]*101300 , 'H2')]
+masse_volumique_out = [cp.PropsSI('Dmass' ,'T', T,'P', P_out[0]*100000 , 'H2')]
 masse_out = [masse_volumique_out[0]*V_out]
 debit_max = 0
 
@@ -27,7 +27,7 @@ while P_out[-1]<500 : #on s'arrete à MP a 500bars
     P= cp.PropsSI('P', 'T', T, 'Dmass', X[-1], 'H2')
     masse_out.append(masse_out[-1]+debit_massique*dt)
     masse_volumique_out.append(masse_out[-1]/V_out)
-    P_out.append((cp.PropsSI('P', 'T', T, 'Dmass', masse_volumique_out[-1], 'H2'))/101300)
+    P_out.append((cp.PropsSI('P', 'T', T, 'Dmass', masse_volumique_out[-1], 'H2'))/100000)
     Pression.append(P/100000)
     Temps.append(Temps[-1]+dt)
 print(Temps[-1])
